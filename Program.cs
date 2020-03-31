9056a8be1288d1b6f8553339a5acc2c935245948
Connor Casselman <ehrem6@gmail.com>
2020-03-31 12:26:20 -0400

    Added ability to return a list of unique words and the amount of times they occur in a paragraph. Add comments throughout for clarity.

--- Program.cs
+++ Program.cs
@@ -8,33 +8,39 @@ namespace Paragraph_Counter
     {        
         static void Main(string[] args)
         {
-            Console.WriteLine("Please enter a paragraph you would like to work with");
-            var input = Console.ReadLine();
-            input = input.ToLower();
-            int menuSelection = PrintMenu();
             Console.Clear();
-            
-
-            switch(menuSelection)
+            bool shouldLoop = true;
+            while(shouldLoop)
             {
-                case 1:
-                    CheckPalindromeWord(input);
-                    break;
-                case 2:
-                    CheckPalindromeSentance(input);
-                    break;
-                case 3:
-                    CheckUniqueWords(input);
-                    break;
-                case 4:
-                    CheckWordsWithLetter(input);
-                    break;
-                default:
-                    break;
-            }   
+                Console.WriteLine("Please enter a paragraph you would like to work with");
+                var input = Console.ReadLine();
+                input = input.ToLower();
+                Console.Clear();
+
+                int menuSelection = PrintMenu();
+                Console.Clear();   
+
+                switch(menuSelection)
+                {
+                    case 1:
+                        CheckPalindromeWord(input);
+                        break;
+                    case 2:
+                        CheckPalindromeSentance(input);
+                        break;
+                    case 3:
+                        CheckUniqueWords(input);
+                        break;
+                    case 4:
+                        CheckWordsWithLetter(input);
+                        break;
+                    default:
+                        break;
+                } 
+            }  
         }
 
-        //Checks to see if string is Palindrome - Currently will fall if there is a special character in a word IE: racecar!
+        //Checks to see what words are a palindrome within the given string type input.
         public static void CheckPalindromeWord(String input)
         {
             int palindromeCount = 0;
@@ -42,47 +48,84 @@ namespace Paragraph_Counter
             //Remove special characters from paragraph - Regex replace with negated set of characters to only replace character outside of what is listed.
             input = Regex.Replace(input, "[^a-zA-Z ]+", "");
 
-            //Count the ammount of Palindromes - See CheckPalindromeCount method
+            //Count the ammount of Palindromes. Split at spaces to get individual words - See CheckPalindromeCount method
             palindromeCount = CheckPalindromeCount(input, ' ');
     
             Console.WriteLine("This Paragraph has "+ palindromeCount + " palindromes in it.");
         }
 
+        //Checks to see what sentances are a palindrome within the given string type input.
         public static void CheckPalindromeSentance(String input)
         {
             int palindromeCount = 0;
             
+            //Remove all spaces from the paragraph.
             input = input.Replace(" ", "");
-            Console.WriteLine(input);
+
+            //Check Palindrome Count by splitting at periods to get full sentances.
             palindromeCount = CheckPalindromeCount(input, '.');
             Console.WriteLine("This Paragraph has "+ palindromeCount + " palindromes in it.");
         }
 
+        //Finds all words within a paragraph and counts the occurance of each. Then list out each unique word found along with the amount of occurances.
         public static void CheckUniqueWords(String input)
         {
-               //Map may be best for this one 
+            //Key = indivudal word Value = the number of occurances of that word.
+            Dictionary<string, int> wordCount = new Dictionary<string, int>();
+
+            //Remove all non alphabetical characters
+            input = Regex.Replace(input, "[^a-zA-Z ]+", "");
+
+            //Loop through the input, splitting at spaces to get individual words.
+            //If the words already exists add 1 to the value - if it does not, add a new key with a value of 1.
+            foreach(String word in input.Split(" "))
+            {
+                if(!wordCount.ContainsKey(word))
+                {
+                    wordCount.Add(word, 1);
+                }
+                else
+                {
+                    wordCount[word]++;
+                }
+            }
+            printDictionary(wordCount);
         }
 
+        //Prints out the key value pairing of the given dictionary.
+        public static void printDictionary(Dictionary<string, int> d)
+        {
+            foreach(KeyValuePair<string, int> pair in d)
+            {
+                Console.WriteLine("{0} = {1}", pair.Key, pair.Value);
+            }
+        }
+
+        //Allows user to input a character - then it will return all the words that contain that letter.
         public static void CheckWordsWithLetter(String input)
         {
             bool shouldLoop = true;
             
             while(shouldLoop)
             {
+                //Input handeling 
                 Console.WriteLine("Please enter a letter - then I will list all the words that contain it: ");
                 String letterInput = Console.ReadLine();
                 letterInput = letterInput.ToLower();
+
+                //Bool for checking that the input is a single character.
                 var isCharacter = (letterInput.Length == 1);
                 
                 if(isCharacter)
                 {
                     Console.WriteLine("Here is a list of words that contain the letter " + letterInput + ": ");
+                    //Loop through paragraph splitting at spaces to get each word. If the word contains the letter list it.
                     foreach(string word in input.Split(' '))
                     {
                         if(word.Contains(letterInput))
                         {
                             //wordList.Add(word);
-                            Console.Write(word + " - "); //<---- Fix here so a '-' doesn't always apear at the end
+                            Console.Write(word + " ");
                         }
                     }
                     shouldLoop = false;
@@ -91,19 +134,24 @@ namespace Paragraph_Counter
                 {
                     Console.WriteLine("Please enter a single character.");
                 } 
+                Console.WriteLine("");
             }
         }
 
-
+        //Method for checking if the given input is a palindrome. Takes a char param to signify where it should cut off the input before checking.
         public static int CheckPalindromeCount(String input, char split)
         {
 
             int count = 0;
+            //For each word in the input, split at the given split character.
             foreach(string word in input.Split(split))
             {
                 bool isPalindrome = false;
+                //Reverse the sttring - See Reverse String method
                 String reversedString = ReverseString(word);
-                if(!string.IsNullOrWhiteSpace(word))
+
+                //Makes sure the returned output isn't null or white space - and greater than 1. Then check if the word and reversedString are equal.
+                if(!string.IsNullOrWhiteSpace(word) && word.Length > 1)
                 {
                     isPalindrome = string.Equals(word, reversedString);
                 }
@@ -111,12 +159,14 @@ namespace Paragraph_Counter
                 if(isPalindrome)
                 {
                     count++;
+                    Console.Write(word + " ");
                 }
             }
+            Console.WriteLine("");
             return count;
         }
 
-//Take a given string then convert to an array of characters - Reverse the order of characters with Array.Reverse then create a new string out of it and return.
+        //Take a given string then convert to an array of characters - Reverse the order of characters with Array.Reverse then create a new string out of it and return.
         public static String ReverseString(String s)
         {
             char[] letters = s.ToCharArray();
@@ -126,20 +176,18 @@ namespace Paragraph_Counter
             return reversedWord;
         }
 
-
-//Print Menu and return the option selected.
+        //Print Menu and return the option selected.
         public static int PrintMenu()
         {
            var printMenu = true;
            var selection = 0;
 
            while(printMenu)
-           {
-                
+           {   
                 Console.WriteLine("What would you like to do?");
-                Console.WriteLine("1. Check if word is a palindrome.");
-                Console.WriteLine("2. Check if a sentance is a palindrome.");
-                Console.WriteLine("3. List count of unique words in a paragraph.");
+                Console.WriteLine("1. Check how many words are a palindrome in the paragraph.");
+                Console.WriteLine("2. Check how many sentances are a palindrome in the paragraph.");
+                Console.WriteLine("3. List a count of unique words in a paragraph.");
                 Console.WriteLine("4. List all words in a paragraph that contain a specific letter.");
 
                 
@@ -162,10 +210,7 @@ namespace Paragraph_Counter
                 }        
             } 
             return selection; 
-        }
-
-            
-            
+        }      
     }
 }
 
